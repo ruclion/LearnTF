669e5130c883524e31baabe6b9fe34f6e3a55742
ruclion <ruclion@163.com>
2017-09-19 08:38:09 +0800

    new problem and solution
    new problem and solution

--- D.py
+++ D.py
@@ -2,14 +2,24 @@ from __future__ import print_function
 #
 #
 
-'a good problem'
+'easy'
 
 __author__ = 'hjkruclion'
 
 import sys
-
+import math
 
 def read_int():
     return list(map(int, sys.stdin.readline().split()))
+def read_str():
+    return sys.stdin.readline().split()[0]
 
-
+N = read_int()[0]
+a = read_int()
+cnt = 0
+for i in range(N):
+    if a[i] % 2 == 0:
+        cnt += 1
+    else:
+        cnt -= 1
+print(int(math.fabs(cnt) + 0.1))
